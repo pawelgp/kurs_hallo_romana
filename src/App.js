e6d525371a66aada96b1acd5c83a7860f8face52
Pawel G Pasich <pawelgp@gmail.com>
2021-04-11 10:04:22 +0200

    switch to npm

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-import logo from './logo.svg'
-import './App.css'
-import List from './components/List'
-import Change from './components/Change'
+import React from 'react'
+import './App.css';
+import List from './components/List';
+// import Change from './components/Change';
 
-function App() {
-  return (
-    <div className="app">
-      <header className="app__header">
-        <img src={logo} className="header__logo" alt="logo" />
-        <p className="header__text">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="header__link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <main>
-        <List />
-        <Change />
-      </main>
-    </div>
-  );
+class App extends React.Component {
+	render() {
+		return (
+			<div className="app">
+				<header className="app__header"></header>
+				<main>
+					<List />
+				</main>
+			</div>
+		);
+	}
 }
-
-export default App
+export default App;
